8610906ca37d0c99a8e576bbaca5fc06ab6f3c61
juronon <uronon23@gmail.com>
2017-10-12 23:19:34 +0300

    added multiply and to divide

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function startCalculate() {
     let expressionFromField = document.getElementById("calculatorField").value;
     expressionFromField = expressionFromField.replace(/\s/g, "");
 
-    let regexp = (/[0-9][-+][0-9]/);
+    let regexp = (/[0-9][-+ */][0-9]/);
 
-    if(expressionFromField.match(regexp)) {
+
+    if (expressionFromField.match(regexp)) {
         document.getElementById("calculationResult").innerHTML = calculatorParser.calculate(expressionFromField);
     } else {
         document.getElementById("calculationResult").innerHTML = "Incorrect data! Try again";
