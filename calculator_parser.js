8e5d96a417483fea05a451af1f418ad75ec6eb17
juronon <uronon23@gmail.com>
2017-10-12 23:18:16 +0300

    added two function multiply and to divide

--- calculator_parser.js
+++ calculator_parser.js
@@ -9,32 +9,46 @@ class CalculatorParser {
 
         if (expression.split("+").length > 1) {
             arrayOfTwoNumbers = expression.split("+");
-
             return this.sum(arrayOfTwoNumbers);
-        } else if (expression.split("-").length > 1) {
+        }
+
+        else if (expression.split("-").length > 1) {
             arrayOfTwoNumbers = expression.split("-");
             return this.subtract(arrayOfTwoNumbers);
         }
 
+        else if (expression.split("*").length > 1) {
+            arrayOfTwoNumbers = expression.split("*");
+            return this.multiply(arrayOfTwoNumbers);
+        }
+
+        else if (expression.split("/").length > 1) {
+            arrayOfTwoNumbers = expression.split("/");
+            return this.to_divide(arrayOfTwoNumbers);
+        }
 
     }
+
 
     sum(arrayOfTwoNumbers) {
         let result = 0;
         let arrayOfTwoNumbersLength = arrayOfTwoNumbers.length;
         for (let i = 0; i < arrayOfTwoNumbersLength; i++) {
             let number = arrayOfTwoNumbers[i];
+
             result += parseInt(number);
         }
 
-        return result;
+        return ' Ваш результат = ' + result;
     }
+
 
     subtract(arrayOfTwoNumbers) {
         let result = 0;
         let arrayOfTwoNumbersLength = arrayOfTwoNumbers.length;
         for (let i = 0; i < arrayOfTwoNumbersLength; i++) {
             let number = arrayOfTwoNumbers[i];
+
             if (i === 0) {
                 result = number;
                 continue;
@@ -42,9 +56,46 @@ class CalculatorParser {
 
             result -= parseInt(number);
         }
-        return result;
-
+        return ' Ваш результат = ' + result;
 
     }
+
+
+    multiply(arrayOfTwoNumbers) {
+        let result = 0;
+        let arrayOfTwoNumbersLength = arrayOfTwoNumbers.length;
+        for (let i = 0; i < arrayOfTwoNumbersLength; i++) {
+            let number = arrayOfTwoNumbers[i];
+
+            if (i === 0) {
+                result = number;
+                continue;
+            }
+
+
+            result *= parseInt(number);
+        }
+
+        return ' Ваш результат = ' + result;
+    }
+
+
+    to_divide(arrayOfTwoNumbers) {
+        let result = 0;
+        let arrayOfTwoNumbersLength = arrayOfTwoNumbers.length;
+        for (let i = 0; i < arrayOfTwoNumbersLength; i++) {
+            let number = arrayOfTwoNumbers[i];
+
+            if (i === 0) {
+                result = number;
+                continue;
+            }
+
+            result /= parseInt(number);
+        }
+
+        return ' Ваш результат = ' + result;
+    }
+
 
 }
